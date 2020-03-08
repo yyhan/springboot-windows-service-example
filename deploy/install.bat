ecd5e4bf301adce9af6b436a8e683d905a4bb866
小天 <cloudin@foxmail.com>
2020-03-08 20:40:39 +0800

    安装和卸载脚本优化
    1、使用 chcp 65001 切换到 utf8 模式运行脚本，避免脚本中中文乱码问题
    2、增加是否以管理员身份运行检测
    3、注释优化

--- deploy/install.bat
+++ deploy/install.bat
@@ -1,8 +1,34 @@
 @echo off
 
-set "BIN_DIR=%CD%"
+chcp 65001
+
+setlocal
+
+echo 【 ==== 开始安装 ==== 】
+
+::::::::::::::::::::::::::::::::::::::::
+:: 检查是否以管理员身份运行
+::::::::::::::::::::::::::::::::::::::::
+fltmc > nul && (
+    echo 正在以管理员身份运行
+)||(
+    echo 请以管理员身份运行!!
+    goto end
+)
+
+::::::::::::::::::::::::::::::::::::::::
+:: 修正根目录
+::::::::::::::::::::::::::::::::::::::::
+cd /d %~dp0
+
+REM 上一级目录作为 home 目录
 cd ..
+
+::::::::::::::::::::::::::::::::::::::::
+:: 变量定义
+::::::::::::::::::::::::::::::::::::::::
 set "HOME_DIR=%CD%"
+set "BIN_DIR=%HOME_DIR%\bin"
 set "LIB_DIR=%HOME_DIR%\lib"
 set "CONFIG_DIR=%HOME_DIR%\config"
 set "TEMP_DIR=%HOME_DIR%\temp"
@@ -18,6 +44,7 @@ set "EXECUTABLE=%BIN_DIR%\${project.service.name}-service_x64.exe"
 
 set "CLASSPATH=%BIN_DIR%\${project.build.finalName}.jar"
 
+rem jvm 路径设置
 rem set "Jvm=C:\jdk1.8.0_181\jre\bin\server\jvm.dll"
 
 set "JVM_OPTS=-Xms750m;-Xmx1024m"
@@ -30,6 +57,9 @@ set "JVM_OPTS=%JVM_OPTS%;-Dlog4j2.configurationFile=%CONFIG_DIR%\log4j2.xml"
 
 set "PROGRAM_ARGS=--spring.config.location=%CONFIG_DIR%\application.properties"
 
+::::::::::::::::::::::::::::::::::::::::
+:: 路径检查和创建
+::::::::::::::::::::::::::::::::::::::::
 if not exist %TEMP_DIR% (
     mkdir %TEMP_DIR%
 )
@@ -37,6 +67,9 @@ if not exist %LOG_DIR% (
     mkdir %LOG_DIR%
 )
 
+::::::::::::::::::::::::::::::::::::::::
+:: 执行安装命令
+::::::::::::::::::::::::::::::::::::::::
 "%EXECUTABLE%" //IS//%SERVICE_NAME% ^
     --Description "%SERVICE_DESC%" ^
     --DisplayName "%SERVICE_DISPLAY_NAME%" ^
@@ -58,4 +91,10 @@ if not exist %LOG_DIR% (
     --JvmOptions "%JVM_OPTS%" ^
     --Startup "%SERVICE_STARTUP_MODE%"
 
+
+:end
+echo 【 ==== 安装结束 ==== 】
+
+REM 按任意键退出
+pause
 
--- deploy/uninstall.bat
+++ deploy/uninstall.bat
@@ -1,13 +1,55 @@
 @echo off
 
-set "BIN_DIR=%CD%"
+chcp 65001
+
+setlocal
+
+echo 【 ==== 开始卸载 ==== 】
+
+
+::::::::::::::::::::::::::::::::::::::::
+:: 检查是否以管理员身份运行
+::::::::::::::::::::::::::::::::::::::::
+fltmc > nul && (
+    echo 正在以管理员身份运行
+)||(
+    echo 请以管理员身份运行!!
+    goto end
+)
+
+
+::::::::::::::::::::::::::::::::::::::::
+:: 修正根目录
+::::::::::::::::::::::::::::::::::::::::
+cd /d %~dp0
+
+REM 上一级目录作为 home 目录
 cd ..
+
+
+::::::::::::::::::::::::::::::::::::::::
+:: 变量定义
+::::::::::::::::::::::::::::::::::::::::
 set "HOME_DIR=%CD%"
+set "BIN_DIR=%HOME_DIR%\bin"
+set "LIB_DIR=%HOME_DIR%\lib"
+set "CONFIG_DIR=%HOME_DIR%\config"
+set "TEMP_DIR=%HOME_DIR%\temp"
+set "LOG_DIR=%HOME_DIR%\logs"
 
 set "SERVICE_NAME=${project.service.name}"
 
 set "EXECUTABLE=%BIN_DIR%\${project.service.name}-service_x64.exe"
 
+
+::::::::::::::::::::::::::::::::::::::::
+:: 执行卸载命令
+::::::::::::::::::::::::::::::::::::::::
 %EXECUTABLE% //DS//%SERVICE_NAME%
 
+
 :end
+echo 【 ==== 卸载结束 ==== 】
+
+REM 按任意键退出
+pause
